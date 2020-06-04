d0f3c6bfda684a0e77e478bf048f37401849d2e8
Alex Moiseev <coding-awm@mail.ru>
2020-06-04 23:15:19 +0600

    подправил скрипт

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 alert('Првиет мир!!');
 console.log("сообщение в консоль");
-let money = 100500;
-let income = 100500;
-let addExpenses = 100500;
-let deposit = 100500;
-let mission = 100500;
-let perio = 100500;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let perio;
