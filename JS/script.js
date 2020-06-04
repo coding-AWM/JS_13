f28f53a62865c543974cb239053ec04c5e8a5a59
Alex Moiseev <coding-awm@mail.ru>
2020-06-04 19:29:45 +0600

    Заполнил скрипт

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
-// alert('Првиет мир!!')
+alert('Првиет мир!!')
+console.log("сообщение в консоль"); -
+let money = 100500;
+let income = 100500;
+let addExpenses = 100500;
+let deposit = 100500;
+let mission = 100500;
+let perio = 100500;
