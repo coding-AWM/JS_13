586ceb12b31830c750d037863854895f7b034d35
Alex Moiseev <coding-awm@mail.ru>
2020-06-05 15:04:39 +0600

    вывел в консоль тип переменных

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,8 @@ let addExpenses = 'аренда, интернет, еда, транспорт';
 let deposit = true;
 let mission = 5000;
 let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
