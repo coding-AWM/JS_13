787db094010eb89fe57bc075bc9a7fe3f04a38d7
Alex Moiseev <coding-awm@mail.ru>
2020-06-05 15:26:41 +0600

    Сделал обязательное задание

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,32 @@
-let money = 300;
-let income = 'Свободный заработок';
-let addExpenses = 'аренда, интернет, еда, транспорт';
-let deposit = true;
-let mission = 5000;
-let period = 6;
+let money;
+let income;
+let addExpensesа;
+let depositrue;
+let mission000;
+let period;
+let lowerAddExpenses;
+let arrAddExpenses;
+let budgetDay;
+
+money = 300;
+income = 'Свободный заработок';
+addExpenses = 'аренда, интернет, еда, транспорт';
+deposit = true;
+mission = 5000;
+period = 6;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-
-console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log(addExpenses.length);
+console.log('"Период равен' + ' ' + period + ' ' + 'месяцев"');
 console.log('Цель заработать' + ' ' + mission + ' ' + 'долларов');
+
+lowerAddExpenses = addExpenses.toLowerCase();
+arrAddExpenses = addExpenses.split(', ');
+
+console.log(arrAddExpenses);
+
+budgetDay = money/30;
+
+console.log(budgetDay);
