f280082f05aa059f3dc327991a442e2727f2e277
Alex Moiseev <coding-awm@mail.ru>
2020-06-05 14:59:34 +0600

    присвоил значения переменным

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 300;
+let income = 'Свободный заработок';
+let addExpenses = 'аренда, интернет, еда, транспорт';
+let deposit = true;
+let mission = 5000;
+let period = 6;
