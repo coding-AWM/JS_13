16366a01cae246f56039f9557f2ab89e74b8344b
Alex Moiseev <coding-awm@mail.ru>
2020-06-05 17:15:11 +0600

    Сделал усложнённый вариант урока02

--- JS/lesson02Hard.js
+++ JS/lesson02Hard.js
@@ -1,4 +1,20 @@
-let num = 266219;
+let num = 266219; //266219
+let stringNum;
+let multiplyNum = 1;
+let powerNum = 0;
+let stringPower;
 
-let stringNum = String(num);
-console.log(stringNum);
+stringNum = String(num);
+
+for (i = 0; i < stringNum.length; i++) {    
+    multiplyNum *= stringNum.charAt(i);   
+}
+console.log(multiplyNum);
+
+powerNum = multiplyNum**3;
+
+stringPower = String(powerNum);
+console.log(stringPower.slice(0, 2));
+
+
+
