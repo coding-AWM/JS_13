e5f28cc35d1e7a93579c8d8959b951d36efaf1f4
Alex Moiseev <coding-awm@mail.ru>
2020-06-08 20:05:16 +0600

    доделал обязательное по уроку 03 усложнённому

--- JS/lesson03Hard.js
+++ JS/lesson03Hard.js
@@ -1,3 +1,88 @@
 'use strict';
 
+let language = document.querySelector('.language');
+let langIndex;
+
 let lang = confirm('Ваш язык общения русский?');
+if (lang) {
+    lang = 'Ru';
+    langIndex = 0;
+} else {
+    lang = 'En';
+    langIndex = 1;
+}
+
+let arrayOfDays = [
+    [
+        'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
+    ],
+    [
+        'Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+    ]
+]
+
+let namePerson = prompt('Как вас зовут?');
+
+console.log('Блок вывода с помощью IF:');
+
+if (lang === 'Ru') {
+    console.log('Понедельник');
+    console.log('Вторник');
+    console.log('Среда');
+    console.log('Четверг');
+    console.log('Пятница');
+    console.log('Суббота');
+    console.log('Воскресенье');
+} else {
+    console.log('Monday');
+    console.log('Thuesday');
+    console.log('Wednesday');
+    console.log('Thursday');
+    console.log('Friday');
+    console.log('Saturday');
+    console.log('Sunday');
+}
+
+console.log('Блок вывода с помощью SWITCH');
+
+switch (lang) {
+    case 'Ru':
+        console.log('Понедельник');
+        console.log('Вторник');
+        console.log('Среда');
+        console.log('Четверг');
+        console.log('Пятница');
+        console.log('Суббота');
+        console.log('Воскресенье');
+        break;
+    case 'En':
+        console.log('Monday');
+        console.log('Thuesday');
+        console.log('Wednesday');
+        console.log('Thursday');
+        console.log('Friday');
+        console.log('Saturday');
+        console.log('Sunday');
+}
+
+console.log('С помощью многомерного массива');
+
+for (let i = 0; i < arrayOfDays[langIndex].length; i++) {
+    console.log(arrayOfDays[langIndex][i]);
+};
+
+let roleOfPerson = namePerson === 'Артём' ? console.log('Директор') :
+    namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
+
+//ниже код для верстки. Это мне нужно, решил сразу проверить. 
+//Понимаю что не по теме и не к уроку, но буду принателен если дашь обратную связь, как можно это упростить
+
+if (lang === 'Ru') {
+    language.classList.add('langRu');      
+} else if (lang === 'En') {
+    language.classList.add('langEn');
+}
+console.log('language: ', language);
+
+//доделаю позже. выводится только понедельник. надо понять как перебрать все элементы в вёрстке
+// да и функция не правильно отображается. 
