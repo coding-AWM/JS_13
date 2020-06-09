9b58443a41fb6109e8e8128f1c09cd39ea0af4a4
Alex Moiseev <coding-awm@mail.ru>
2020-06-09 23:51:07 +0600

    подсправил урок 4 усложнённый. навёл порядок на ГИТе

--- JS/lesson04Hard.js
+++ JS/lesson04Hard.js
@@ -1,8 +1,5 @@
-// let message = prompt('Напишите произвольное предложение');
-// let length = prompt('Напишите цифрами длинну сообщения, которую надо оставить');
-
-message = 5555;
-length = 5;
+message = '123456789012345678901234567890123';
+length = 30;
 
 function cutOff (mess, leng) {
 
@@ -14,4 +11,4 @@ function cutOff (mess, leng) {
         return mess.slice(0, leng);
     }    
 }
-console.log(cutOff(message, length) + ' ' + '. . . ');
+console.log(cutOff(message, length) + ' ' + '... ');
