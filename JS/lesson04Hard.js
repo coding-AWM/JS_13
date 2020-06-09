4c29de715730f412fe36144e86ed5ddf836780a4
Alex Moiseev <coding-awm@mail.ru>
2020-06-09 16:49:36 +0600

    the lesson04Hard is almost over4

--- JS/lesson04Hard.js
+++ JS/lesson04Hard.js
@@ -1,8 +1,8 @@
-let message = prompt('Напишите произвольное предложение');
-let length = prompt('Напишите цифрами длинну сообщения, которую надо оставить');
+// let message = prompt('Напишите произвольное предложение');
+// let length = prompt('Напишите цифрами длинну сообщения, которую надо оставить');
 
-// message = 5555;
-// length = 5;
+message = 5555;
+length = 5;
 
 function cutOff (mess, leng) {
 
@@ -12,16 +12,6 @@ function cutOff (mess, leng) {
     }  else {
         console.log('TRUE');
         return mess.slice(0, leng);
-    }
-    // else {
-    //     alert('Введите текствотове сообщение');
-    // }
-    // return mess.slice(0, leng);
+    }    
 }
 console.log(cutOff(message, length) + ' ' + '. . . ');
-// console.log(typeof cutOff(message, length));
-// console.log(typeof (cutOff(message, length)) === typeof '');
-
-//Затыка. если раскоментировать то пишет переполнение стека. час уже сижу бьюсь. 
-//в выводах в конце. проверка  работает. треотий лог. как его пихаю в ИФ, всё. куда копать то?
-//да и проверка показыает что даже математические знаки она делает строкой. 
