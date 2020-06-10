296390354288f15a6e116658287ada13ca4e5200
Alex Moiseev <coding-awm@mail.ru>
2020-06-10 13:22:19 +0600

    Finish lesson05

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,85 @@
 'use strict'
-let money = 1 * (prompt('Ваш месячный доход?'));
-let income = 1 * (prompt('Свободный заработок'));
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let expense1 = prompt('Введите обязательную статью расходов?');
-let amount1 = 1 * (prompt('Во сколько это обойдётся?'));
-let expense2 = prompt('Введите обязательную статью расходов?');
-let amount2 = 1 * (prompt('Во сколько это обойдётся?'));
-let mission = 5000;
-let period = 6;
-let arrAddExpenses = addExpenses.toLowerCase().split(', ');
-let summaryIncome = sum(money, income);
-let accumulatedMonth = getAccumulatedMonth();
 
-function sum(a, b) {
+let isNumber = function (z) {
+    return !isNaN(parseFloat(z)) && isFinite(z);
+}
+
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission = 2000,
+    period = 6;
+
+let start = function () {
+    // while (isNaN(money) || money.trim() === '' || money === null) {
+    //     money = prompt('Ваш месячный доход?');
+    // }
+    //Код выше для проверки на вводимые данные. всё что не число FALSE
+
+    // while (isNaN(parseFloat(money))) {
+    //     money = +prompt('Ваш месячный доход?');
+    // }
+    //код выше проверяет ЭтоЧисло, а перед этим приводит к дробному числу если это число то код продолжается.
+    //Если же не число (а всё что не число не получится запарсить в число). то код повторяется
+
+    while (!isNumber(money)) {
+        money = prompt('Ваш месячный доход?');
+    }
+
+    while (!isNumber(income)) {
+        income = prompt('Свободный заработок');
+    }
+    //два кода выше тоже самое. но создана универсальная функция для проверки на число
+
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+    deposit = confirm('Есть ли у вас депозит в банке?');
+};
+start();
+
+let arrAddExpenses = addExpenses.toLowerCase().split(', ');
+let summaryIncome = summ(+money, +income);
+let expenses = [];
+
+function summ(a, b) {
     return a + b;
 }
 
-function getExpensesMonth() {
-    return amount1 + amount2;
+let getExpensesMonth = function () {
+    let sum = 0;
+    for (let i = 0; i < 2; i++) {
+        expenses[i] = prompt('Введите обязательную статью расходов?');
+        sum += +prompt('Во сколько это обойдётся?');
+    }
+    console.log('Список ужемесячных трат: ', expenses);
+    return sum;
 }
+
+let expensesAmount = getExpensesMonth();
+console.log('Расходы за месяц: ', expensesAmount);
 
 function getAccumulatedMonth() {
-    return summaryIncome - getExpensesMonth();
+    return summaryIncome - expensesAmount;
 }
 
+let accumulatedMonth = getAccumulatedMonth();
+console.log('Чистый доход в месяц: ', accumulatedMonth);
+
 function getTargetMonth() {
-    return Math.ceil(mission / getAccumulatedMonth());
+    if (Math.ceil(mission / accumulatedMonth) > 0) {
+        console.log('Цель будет достигнута за: ');
+        return Math.ceil(mission / accumulatedMonth);
+    } else {
+        console.log('С отрицательным расходом цель достигнута не будет: ');
+    }
 }
 let budgetDay = Math.floor(accumulatedMonth / 30);
 
-function showDetailes () {
-    console.log('Расходы за месяц составили: ' + getExpensesMonth() + ' долларов');
+function showDetailes() {
+    console.log('Расходы за месяц составили: ' + expensesAmount + ' долларов');
     console.log('Бюджет на месяц: ' + getAccumulatedMonth() + ' долларов!');
     console.log('Цель заработать' + ' ' + mission + ' ' + ' долларов');
-    console.log('Цель будет достигнута за:' + ' ' + getTargetMonth() + ' ' + 'месяцев');
+    console.log( getTargetMonth() + ' ' + 'месяца');
     console.log('Бюджет на день ' + budgetDay + ' долларов');
     console.log(arrAddExpenses);
 }
