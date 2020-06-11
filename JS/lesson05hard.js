dae5745f9886cd834e50495c84ac1798c9926a05
Alex Moiseev <coding-awm@mail.ru>
2020-06-11 10:12:53 +0600

    начал делать цикл 100

--- JS/lesson05hard.js
+++ JS/lesson05hard.js
@@ -10,20 +10,20 @@ arr[5] = '654987321';
 arr[6] = '951753456';
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] == 2 || arr[i][0] ==  4) {
+    if (arr[i][0] == 2 || arr[i][0] == 4) {
         console.log(arr[i]);
     }
 }
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        for (let k = 1; k < 10; k++) {
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         for (let k = 1; k < 10; k++) {
 
-            if (+arr[i][j] % +arr[i][j] === 0 && +arr[i][j] / +arr[i][j] === 1 && +arr[i][j] / k === 1 && +arr[i][j] % 0)
-            console.log(arr[i][j]);
-        }
-    }
-}
+//             if (+arr[i][j] % +arr[i][j] === 0 && +arr[i][j] / +arr[i][j] === 1 && +arr[i][j] / k === 1 && +arr[i][j] % 0)
+//             console.log(arr[i][j]);
+//         }
+//     }
+// }
 // дорабооать цикл. для вывода простых чисел. первая чстьработает, надо сделать проверку на деление на все числа
 
 
@@ -31,3 +31,15 @@ for (let i = 0; i < arr.length; i++) {
 // if ( 4 % 3 === 0 && 4 / 4 === 1) {
 //     console.log(5);
 // }
+
+for (let i = 1; i <= 100; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            break
+        } else {
+
+            console.log(i);
+            break
+        }
+    }
+}
