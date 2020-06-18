a8c1b1d7f7730db06f96eb9146522360ea277353
Alex Moiseev <coding-awm@mail.ru>
2020-06-18 06:16:04 +0600

    Исправил вызов в уроке09

--- JS/less09.js
+++ JS/less09.js
@@ -1,17 +1,19 @@
 'use strict'
 
-let buttonCalculate = document.querySelector('#start');
-let addIncomeButton = document.querySelector('button.income_add');
-let addExpensesButton = document.querySelector('button.expenses_add');
-let depositCheck = document.querySelector('#deposit-check')
+let buttonCalculate = document.getElementById('start');
+let addIncomeButton = document.getElementsByTagName('button')[0];
+let addExpensesButton = document.getElementsByTagName('button')[1];
+let depositCheck = document.querySelector('#deposit-check');
 let addIncomeItems = document.querySelectorAll('.additional_income-item');
-let budgetMonthValue = document.querySelector('.budget_month-value');
-let budgetDayValue = document.querySelector('.budget_day-value');
-let expensesMonthValue = document.querySelector('.expenses_month-value');
-let additionalIncomeValue = document.querySelector('.additional_income-value');
-let additionaExpensesValue = document.querySelector('.additional_expenses-value');
-let incomePeriodValue = document.querySelector('.income_period-value');
-let targetMonthValue = document.querySelector('.target_month-value');
+
+let budgetMonthValue = document.getElementsByClassName('result-total')[0];
+let budgetDayValue = document.getElementsByClassName('result-total')[1];
+let expensesMonthValue = document.getElementsByClassName('result-total')[2];
+let additionalIncomeValue = document.getElementsByClassName('result-total')[3];
+let additionaExpensesValue = document.getElementsByClassName('result-total')[4];
+let incomePeriodValue = document.getElementsByClassName('result-total')[5];
+let targetMonthValue = document.getElementsByClassName('result-total')[6];
+
 let salaryAmount = document.querySelector('.salary-amount');
 let incomeTitle = document.querySelector('.income-title');
 let incomeAmount = document.querySelector('.income-amount');
